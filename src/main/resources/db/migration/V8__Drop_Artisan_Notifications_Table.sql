0e2f7405d251210db4a415203da59107825b9c2e
VuJason <minhtrong220703@gmail.com>
2026-03-09 16:09:06 +0700

    feat: custom order flow and integrate VNPay, ZaloPay

--- src/main/resources/db/migration/V8__Drop_Artisan_Notifications_Table.sql
+++
@@ -1,7 +0,0 @@
--- Drop artisan_notifications table
--- This table is replaced by universal notifications table
-
-DROP TABLE IF EXISTS artisan_notifications CASCADE;
-
--- Add comment
-COMMENT ON TABLE notifications IS 'Universal notification system - replaces artisan_notifications';
