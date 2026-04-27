16d4b54f4767af8233ddcc437e0de85740a0d924
VuJason <minhtrong220703@gmail.com>
2026-04-28 00:10:40 +0700

    fix: feedback for product and map estimate day for stage

--- src/main/resources/db/migration/V4__add_order_detail_to_feedback.sql
+++
@@ -1,9 +0,0 @@
--- Add order_detail_id column to feedbacks table
-ALTER TABLE feedbacks ADD COLUMN order_detail_id UUID;
-
--- Add foreign key constraint
-ALTER TABLE feedbacks ADD CONSTRAINT fk_feedback_order_detail 
-    FOREIGN KEY (order_detail_id) REFERENCES order_detail(id) ON DELETE CASCADE;
-
--- Add index for better query performance
-CREATE INDEX idx_feedback_order_detail ON feedbacks(order_detail_id);
